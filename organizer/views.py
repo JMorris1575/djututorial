4cd21ac5c993baab7fc60b7981240f1d804e20a6
JMorris1575 <FrJamesMorris@gmail.com>
2016-02-07 21:57:04 -0500

    After section 5.8

--- organizer/views.py
+++ organizer/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import (
     get_object_or_404, render)
 
-from .models import Tag
+from .models import Startup, Tag
 
 
-def homepage(request):
+def startup_detail(request, slug):
+    startup = get_object_or_404(
+        Startup, slug__iexact=slug)
+    return render(
+        request,
+        'organizer/startup_detail.html',
+        {'startup': startup})
+
+
+def startup_list(request):
+    return render(
+        request,
+        'organizer/startup_list.html',
+        {'startup_list': Startup.objects.all()})
+
+
+def tag_list(request):
     return render(
         request,
         'organizer/tag_list.html',
