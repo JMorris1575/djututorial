0d71c428970211927d324ba943314229e25705ab
Jim <FrJamesMorris@gmail.com>
2016-02-15 21:23:31 -0500

    After Section 9.2.5

--- organizer/urls.py
+++ organizer/urls.py
@@ -1,13 +1,20 @@
 from django.conf.urls import url
 
 from .views import(
-        TagCreate, startup_detail, startup_list,
-        tag_detail, tag_list)
+        NewsLinkCreate, StartupCreate, TagCreate,
+        startup_detail, startup_list, tag_detail,
+        tag_list)
 
 urlpatterns = [
+    url(r'^newslink/create/$',
+        NewsLinkCreate.as_view(),
+        name='organizer_newslink_create'),
     url(r'^startup/$',
         startup_list,
         name='organizer_startup_list'),
+    url(r'^startup/create/$',
+        StartupCreate.as_view(),
+        name='organizer_startup_create'),
     url(r'^startup/(?P<slug>[\w\-]+)/$',
         startup_detail,
         name='organizer_startup_detail'),
