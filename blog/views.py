ff72c2fa83a4a65b17b2e079ccc80aa60ba07c68
Jim <FrJamesMorris@gmail.com>
2016-02-16 14:17:03 -0500

    After Section 9.3.1

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import (
     get_object_or_404, redirect, render)
+
 from django.views.generic import View
 
 from .models import Post
 
 from .forms import PostForm
-
 
 
 class PostCreate(View):
@@ -29,6 +29,7 @@ class PostCreate(View):
                 self.template_name,
                 {'form': bound_form})
 
+
 def post_detail(request, year, month, slug):
     post = get_object_or_404(
         Post,
@@ -49,9 +50,43 @@ class PostList(View):
             'blog/post_list.html',
             {'post_list': Post.objects.all()})
 
-def post_list(request):
-    return render(
-        request,
-        'blog/post_list.html',
-        {'post_list': Post.objects.all()})
-        
+
+
+class PostUpdate(View):
+    form_class = PostForm
+    model = Post
+    template_name = 'blog/post_form_update.html'
+
+    def get_object(self, year, month, slug):
+        return get_object_or_404(
+            self.model,
+            pub_date__year=year,
+            pub_date__month=month,
+            slug=slug)
+
+    def get(self, request, year, month, slug):
+        post = self.get_object(self, request, year, month, slug)
+        context = {
+            'form': self.form_class(
+                instance=post),
+            'poar': post,
+        }
+        return render(
+            request, self.template_name, context)
+
+    def post(self, request, year, month, slug):
+        post = self.get_object(self, request, year, month, slug)
+        bound_form = self.form_class(
+            request.POST, instance=post)
+        if bound_form.is_valid():
+            new_post = bound_form.save()
+            return redirect(new_post)
+        else:
+            context = {
+                'form': bound_form,
+                'post': post,
+            }
+            return render(
+                request,
+                self.template_name,
+                context)
